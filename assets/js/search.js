a8bd4d30ff0f970749a2d46f3004510effced6f5
hemantdidel <hemantdidel2002@gmail.com>
2026-08-05 18:01:11 +0530

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,35 @@
+// ==============================
+// Batch Search
+// ==============================
 
+const searchInput = document.getElementById("searchBatch");
+
+if (searchInput) {
+
+    searchInput.addEventListener("keyup", function () {
+
+        const value = this.value.toLowerCase();
+
+        const cards = document.querySelectorAll(".batch-card");
+
+        cards.forEach(card => {
+
+            const title = card.querySelector("h2").textContent.toLowerCase();
+
+            const desc = card.querySelector("p").textContent.toLowerCase();
+
+            if (title.includes(value) || desc.includes(value)) {
+
+                card.style.display = "block";
+
+            } else {
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+}
